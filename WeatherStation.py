ea710b32956c6328de1780695ce5eb77cd0b9d24
chillybones <jayrygeo@pm.me>
2019-01-20 13:28:28 -0700

    Timestamp

--- WeatherStation.py
+++ WeatherStation.py
@@ -41,7 +41,8 @@ while True:
 
         # Only need one time stamp between both pulls becuase the 5-day and the current will be updated at the same
         # time when needed.
-        lastUpdate = datetime.strptime(str(datetime.now()), '%H:%M')
+        timeStamp = datetime.strptime(str(datetime.now()), '%Y-%m-%dT%H:%M:%S.%f')
+        lastUpdate = timeStamp.strptime('%H:%M')
 
         print(currentWeather)
         print("Last update at: %s" % lastUpdate)

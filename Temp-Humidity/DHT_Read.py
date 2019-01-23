7afab866b21e7fa519ad9a0cd6d4abe9938efb1e
chillybones <jayrygeo@pm.me>
2019-01-23 16:56:01 -0700

    initial commit

---
+++ Temp-Humidity/DHT_Read.py
@@ -0,0 +1,9 @@
+import Adafruit_DHT
+
+sensor = Adafruit_DHT.DHT11
+
+pin = 21
+
+humid, temp = Adafruit_DHT.read_retry(sensor, pin)
+
+print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))

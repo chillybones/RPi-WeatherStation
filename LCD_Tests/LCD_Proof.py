6b68beff082611a7fb9e7d0af516cd6f33a89c45
chillybones <jayrygeo@pm.me>
2019-01-23 14:47:41 -0700

    Changing to 4x20 screen

--- LCD_Tests/LCD_Proof.py
+++ LCD_Tests/LCD_Proof.py
@@ -4,7 +4,7 @@ from time import sleep
 
 # When numbering_mode is set to BOARD, reference PIN number.
 # When numbering_mode is set to BCM, reference the number after GP in the pinout name.
-lcd = CharLCD(pin_rs=15, pin_rw=18, pin_e=16, pins_data=[8, 10, 11, 12], numbering_mode=GPIO.BOARD, cols=16, rows=2, dotsize=8)
+lcd = CharLCD(pin_rs=15, pin_rw=18, pin_e=16, pins_data=[8, 10, 11, 12], numbering_mode=GPIO.BOARD, cols=20, rows=4, dotsize=8)
 
 # Clear the screen of any previous input
 lcd.clear()
@@ -26,7 +26,7 @@ while(True):
     # This is a symptom of the LCD screen and printed character 'hanging' in memory.
     # If you don't overwrite EVERY character space on a row, anything extending past
     # the last char of the new row will remain.
-    while(16 > len(secondRow)):
+    while(20 > len(secondRow)):
             secondRow = secondRow + u' '
 
     lcd.write_string(str(secondRow))

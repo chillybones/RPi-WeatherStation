a0c32ef97ec8986b777f1fe22cf05d7054fe265b
chillybones <jayrygeo@pm.me>
2019-01-23 16:59:19 -0700

    Test successful, added sleep

--- Temp-Humidity/DHT_Read.py
+++ Temp-Humidity/DHT_Read.py
@@ -1,9 +1,12 @@
 import Adafruit_DHT
+from time import sleep
 
 sensor = Adafruit_DHT.DHT11
 
 pin = 21
 
-humid, temp = Adafruit_DHT.read_retry(sensor, pin)
 
-print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temp, humid))
+while True:
+    humid, temp = Adafruit_DHT.read_retry(sensor, pin)
+    print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temp, humid))
+    sleep(1)
